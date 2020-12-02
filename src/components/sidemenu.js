fc6a4e52fccb474069ca5719df0cef1773a1a9bc
Dani Ramírez <>
2020-12-02 11:20:02 +0100

    updating repo

--- src/components/sidemenu.js
+++ src/components/sidemenu.js
@@ -137,6 +137,28 @@ const SideMenu = () => (
         </div>
       </div>
       <div className="flex-shrink-0 block w-full">
+        <div className="flex-shrink-0 flex border-t border-gray-200 p-4">
+          <a href="#" className="flex-shrink-0 group block">
+            <div className="flex items-center">
+              <div>
+                <img
+                  className="inline-block h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+              </div>
+              <div className="ml-3">
+                <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">
+                  Tom Cook
+                </p>
+                <p className="text-sm font-medium text-gray-500 group-hover:text-gray-700">
+                  View profile
+                </p>
+              </div>
+            </div>
+          </a>
+        </div>
+
         <a className="group rounded-md py-2 px-4 flex items-center text-sm font-medium text-gray-600 hover:text-gray-900 hover:bg-gray-50">
           <svg
             className="mr-3 h-6 w-6 text-gray-400 group-hover:text-gray-500"
