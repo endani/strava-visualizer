a5721adbb50c7935e0e5c22fba036c41ac0f0dad
Dani Ramírez <dani.ramirez@payemcard.com>
2024-05-26 16:30:43 +0200

    feat(DAN-12): refactor to NextUI

--- src/components/atoms/chip/chip.types.ts
+++
@@ -1,9 +0,0 @@
-import { TailwindColor } from '../button/button.types'
-
-export interface ChipProps {
-  children: React.ReactNode
-  className?: string
-  color?: TailwindColor
-  disabled?: boolean
-  onClick?: () => void
-}
--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { ActivityCard } from './activity-card/activity-card'
-export { ActivityFilters } from './activity-filters/activity-filters'
-export { Button } from './atoms/button/button'
-export { LoadingAlert } from './loading-alert/loading-alert'
-export { Input } from './atoms/input/input'
