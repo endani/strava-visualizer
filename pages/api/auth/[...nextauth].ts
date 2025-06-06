70dcaabcfd00e34184c5d570d399f586fedf332e
Dani Ramírez <dani@creativesymbol.net>
2025-06-06 15:59:43 +0200

    adds debug

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,11 @@
 import NextAuth from 'next-auth'
 import StravaProvider from 'next-auth/providers/strava'
 
+console.log('Strava Client ID:', process.env.STRAVA_CLIENT_ID)
+console.log('Strava Client Secret is set:', !!process.env.STRAVA_CLIENT_SECRET)
+console.log('NextAuth Secret is set:', !!process.env.NEXTAUTH_SECRET)
+console.log('NEXTAUTH_URL is set:', process.env.NEXTAUTH_URL)
+
 export default NextAuth({
   providers: [
     StravaProvider({
