a05f5f9a2be797111fd1cda60e756859b90088bb
Dani Ramírez <dani@creativesymbol.net>
2025-06-06 16:23:16 +0200

    adds debug

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,38 +7,39 @@ console.log('NextAuth Secret is set:', !!process.env.NEXTAUTH_SECRET)
 console.log('NEXTAUTH_URL is set:', process.env.NEXTAUTH_URL)
 
 export default NextAuth({
-  providers: [
-    // StravaProvider({
-    //   clientId: process.env.STRAVA_CLIENT_ID,
-    //   clientSecret: process.env.STRAVA_CLIENT_SECRET,
-    //   authorization: {
-    //     params: {
-    //       scope: 'activity:read_all,profile:read_all',
-    //     },
-    //   },
-    // }),
-  ],
+  providers: [],
   secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token
-        token.refreshToken = account.refresh_token
-        token.expiresAt = account.expires_at
-      }
+  // providers: [
+  //   // StravaProvider({
+  //   //   clientId: process.env.STRAVA_CLIENT_ID,
+  //   //   clientSecret: process.env.STRAVA_CLIENT_SECRET,
+  //   //   authorization: {
+  //   //     params: {
+  //   //       scope: 'activity:read_all,profile:read_all',
+  //   //     },
+  //   //   },
+  //   // }),
+  // ],
+  // callbacks: {
+  //   async jwt({ token, account }) {
+  //     // Persist the OAuth access_token to the token right after signin
+  //     if (account) {
+  //       token.accessToken = account.access_token
+  //       token.refreshToken = account.refresh_token
+  //       token.expiresAt = account.expires_at
+  //     }
 
-      return token
-    },
-    async session({ session, token }) {
-      // Send properties to the client, like an access_token from a provider.
-      session.accessToken = token.accessToken
+  //     return token
+  //   },
+  //   async session({ session, token }) {
+  //     // Send properties to the client, like an access_token from a provider.
+  //     session.accessToken = token.accessToken
 
-      return session
-    },
-  },
-  pages: {
-    signIn: '/auth/signin',
-    error: '/auth/error',
-  },
+  //     return session
+  //   },
+  // },
+  // pages: {
+  //   signIn: '/auth/signin',
+  //   error: '/auth/error',
+  // },
 })
