3077fddbf8159980756f09cde97846f9901db63c
Dani Ramírez <dani@creativesymbol.net>
2026-03-01 19:10:36 +0100

    fix strava auth

---
+++ pages/api/auth/signin/strava.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import NextAuthHandler from '@/lib/nextauth'
+
+export const config = { runtime: 'nodejs' }
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  req.query = { ...req.query, nextauth: ['signin', 'strava'] }
+
+  return NextAuthHandler(req, res)
+}
